d47a27126dc15fa1f7338b505dd67a01fa82aeb8
Jason Ho <jason.ho@safecode.com.au>
2018-01-11 10:25:42 +1100

    Re-arrange sections and add more columns to table LP_Customer for description

--- Main.sql
+++ Main.sql
@@ -36,26 +36,6 @@ ALTER TABLE [dbo].[EmployerFrequency] CHECK CONSTRAINT [FK_EmployerFrequency_Fre
 GO
 /****** ****************************************************************************** ******/
 
-/****** Object:  Table [dbo].[LP_Customer]    Script Date: 21/12/2017 8:50:58 AM ******/
-ALTER TABLE LP_Customer ADD HasHECSDebt bit
-ALTER TABLE LP_Customer ADD IsClaimingTax bit
-ALTER TABLE LP_Customer ADD IsStaffSpecialist bit
-ALTER TABLE LP_Customer ADD IsMinisterReligion bit
-ALTER TABLE LP_Customer ADD RealBaseSalary decimal(18,2)
-
-/** NOT READY TO PUBLISH YET **/
-ALTER TABLE LP_Customer ADD GenCapIncGst decimal(18,2)
-ALTER TABLE LP_Customer ADD GenCapExcGst decimal(18,2)
-ALTER TABLE LP_Customer ADD MealEntertainment decimal(18,2)
-ALTER TABLE LP_Customer ADD Superannuation decimal(18,2)
-ALTER TABLE LP_Customer ADD OtherExempt decimal(18,2)
-ALTER TABLE LP_Customer ADD RemoteAreaRent decimal(18,2)
-ALTER TABLE LP_Customer ADD RemoteAreaMortgage decimal(18,2)
-ALTER TABLE LP_Customer ADD RemoteAreaFuel decimal(18,2)
-ALTER TABLE LP_Customer ADD OtherPreTaxItem decimal(18,2)
-ALTER TABLE LP_Customer ADD OtherPosTaxItem decimal(18,2)
-/****** ****************************************************************************** ******/
-
 /****** Object:  StoredProcedure [dbo].[sp_lp_GetEmployerById]    Script Date: 12/12/2017 1:32:29 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -242,6 +222,155 @@ SELECT  er.ErId,
 FROM    dbo.Employers er
 inner join EmployersExt erext on erext.erid = er.erid
 GO
+/****** ****************************************************************************** ******/
+
+/****** Object:  StoredProcedure [dbo].[sp_LP_UpdateEmployer]    Script Date: 12/12/2017 2:31:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[sp_LP_UpdateEmployer]
+	
+	@erid int,
+	@email nvarchar(510),
+	@phone nvarchar(510),
+	@fax nvarchar(510),
+	@address1 nvarchar(510),
+	@address2 nvarchar(510),
+	@address3 nvarchar(510),
+	@city nvarchar(510),
+	@state nvarchar(510),
+	@postcode nvarchar(510),
+	@gst bit,
+	@fbt bit,
+	@quoting int,
+	@partnerID nvarchar(200),
+    @Website nvarchar(50),
+	@Abn nvarchar(100),
+	@Acn nvarchar(100),
+	@AssignedTo nvarchar(100),
+	@Address nvarchar(100),
+	@CityPhis nvarchar(50),
+	@PostCodePhis nvarchar(50),
+	@StatePhis nvarchar(20),
+	@Industry nvarchar(100),
+	@ParentCompanyID int,
+	@AboutCompany nvarchar(MAX),
+	@longitude decimal(10,6),
+	@latitude decimal(10,6),
+	@status bit,
+	@BrokerId int,
+	@AgentId int,
+	@InvoicingPartnerId int = 0,  --No longer used
+	@PackagingProviderId int = 0,
+	@EmployerTypeId int = NULL,
+	@MaximumLeases int = NULL,
+	@IsGstDeducted bit = 0,
+	@IsGstEquivalent bit = 0,
+	@IsItcRefunded bit = 0,
+	@AllowLuxuryVehicle bit = 0,
+	@LuxuryVehicleCharged bit = 0,
+	@AllowOperationCost bit = 0,
+	@AllowExtraFields bit = 0
+AS
+
+BEGIN
+	
+	begin try
+	
+		begin transaction
+	
+		update employers
+		set email = @email,
+			PhoneWork = @phone,
+			FAX = @fax,
+			PostalAddress1 = @address1,
+			PostalAddress2 = @address2,
+			PostalAddress3 = @address3,
+			PostalCity = @city,
+			PostalPostCode = @postcode,
+			PostalState = @state,
+			ParentID = @ParentCompanyID,
+			EmployerTypeId = @EmployerTypeId,
+			MaximumLeases = @MaximumLeases,
+			IsGstDeducted = @IsGstDeducted,
+			IsGstEquivalent = @IsGstEquivalent,
+			IsItcRefunded = @IsItcRefunded,
+			AllowLuxuryVehicle = @AllowLuxuryVehicle,
+			LuxuryVehicleCharged = @LuxuryVehicleCharged,
+			AllowOperationCost = @AllowOperationCost,
+			AllowExtraFields = @AllowExtraFields
+		where erid = @erid
+		
+		UPDATE EmployersExt
+		SET GSTPaidInDeduction = @gst, 
+			CollectFBT = @fbt, 
+			NovatedLeasingType = @quoting,
+			PartnerID = @partnerID,
+			Website = @Website,
+			Abn = @Abn,
+			Acn = @Acn,
+			AssignedTo = @AssignedTo,
+			Address = @Address,
+			City = @CityPhis,
+			PostCode = @PostCodePhis,
+			State = @StatePhis,
+			Industry = @Industry,
+			AboutCompany = @AboutCompany,
+			longitude = @longitude,
+			latitude = @latitude,
+			Active = @status,
+			Brokerid = @BrokerId,
+			AgentId = @AgentId,
+			PackagingProviderId = @PackagingProviderId
+		WHERE Erid =  @erid
+
+		commit transaction
+	end try
+	begin catch
+		if @@trancount > 0
+			rollback
+	
+		-- Raise an error with the details of the exception
+		DECLARE @ErrMsg nvarchar(4000), @ErrSeverity int
+		SELECT	@ErrMsg = ERROR_MESSAGE(),
+				@ErrSeverity = ERROR_SEVERITY()
+		
+		RAISERROR(@ErrMsg, @ErrSeverity, 1)
+	end catch
+END
+/****** ****************************************************************************** ******/
+
+/****** Object:  Table [dbo].[LP_Customer]    Script Date: 21/12/2017 8:50:58 AM ******/
+ALTER TABLE LP_Customer ADD HasHECSDebt bit
+ALTER TABLE LP_Customer ADD IsClaimingTax bit
+ALTER TABLE LP_Customer ADD IsStaffSpecialist bit
+ALTER TABLE LP_Customer ADD IsMinisterReligion bit
+ALTER TABLE LP_Customer ADD RealBaseSalary decimal(18,2)
+
+/** NOT READY TO PUBLISH YET **/
+ALTER TABLE LP_Customer ADD GenCapIncGst decimal(18,2)
+ALTER TABLE LP_Customer ADD GenCapExcGst decimal(18,2)
+ALTER TABLE LP_Customer ADD MealEntertainment decimal(18,2)
+ALTER TABLE LP_Customer ADD Superannuation decimal(18,2)
+ALTER TABLE LP_Customer ADD OtherExempt decimal(18,2)
+ALTER TABLE LP_Customer ADD RemoteAreaRent decimal(18,2)
+ALTER TABLE LP_Customer ADD RemoteAreaMortgage decimal(18,2)
+ALTER TABLE LP_Customer ADD RemoteAreaFuel decimal(18,2)
+ALTER TABLE LP_Customer ADD OtherPreTaxItem decimal(18,2)
+ALTER TABLE LP_Customer ADD OtherPosTaxItem decimal(18,2)
+
+ALTER TABLE LP_Customer ADD GenCapIncGstDesc varchar(255)
+ALTER TABLE LP_Customer ADD GenCapExcGstDesc varchar(255)
+ALTER TABLE LP_Customer ADD MealEntertainmentDesc varchar(255)
+ALTER TABLE LP_Customer ADD SuperannuationDesc varchar(255)
+ALTER TABLE LP_Customer ADD OtherExemptDesc varchar(255)
+ALTER TABLE LP_Customer ADD RemoteAreaRentDesc varchar(255)
+ALTER TABLE LP_Customer ADD RemoteAreaMortgageDesc varchar(255)
+ALTER TABLE LP_Customer ADD RemoteAreaFuelDesc varchar(255)
+ALTER TABLE LP_Customer ADD OtherPreTaxItemDesc varchar(255)
+ALTER TABLE LP_Customer ADD OtherPosTaxItemDesc varchar(255)
 /****** ****************************************************************************** ******/
 
 /****** Object:  StoredProcedure [dbo].[sp_lp_GetQuoteCustomerById]    Script Date: 20/12/2017 10:43:41 AM ******/
@@ -470,124 +599,6 @@ AS
 		@otherPosTaxItem)
 	set @ID = scope_identity()
 GO
-/****** ****************************************************************************** ******/
-
-/****** Object:  StoredProcedure [dbo].[sp_LP_UpdateEmployer]    Script Date: 12/12/2017 2:31:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-ALTER PROCEDURE [dbo].[sp_LP_UpdateEmployer]
-	
-	@erid int,
-	@email nvarchar(510),
-	@phone nvarchar(510),
-	@fax nvarchar(510),
-	@address1 nvarchar(510),
-	@address2 nvarchar(510),
-	@address3 nvarchar(510),
-	@city nvarchar(510),
-	@state nvarchar(510),
-	@postcode nvarchar(510),
-	@gst bit,
-	@fbt bit,
-	@quoting int,
-	@partnerID nvarchar(200),
-    @Website nvarchar(50),
-	@Abn nvarchar(100),
-	@Acn nvarchar(100),
-	@AssignedTo nvarchar(100),
-	@Address nvarchar(100),
-	@CityPhis nvarchar(50),
-	@PostCodePhis nvarchar(50),
-	@StatePhis nvarchar(20),
-	@Industry nvarchar(100),
-	@ParentCompanyID int,
-	@AboutCompany nvarchar(MAX),
-	@longitude decimal(10,6),
-	@latitude decimal(10,6),
-	@status bit,
-	@BrokerId int,
-	@AgentId int,
-	@InvoicingPartnerId int = 0,  --No longer used
-	@PackagingProviderId int = 0,
-	@EmployerTypeId int = NULL,
-	@MaximumLeases int = NULL,
-	@IsGstDeducted bit = 0,
-	@IsGstEquivalent bit = 0,
-	@IsItcRefunded bit = 0,
-	@AllowLuxuryVehicle bit = 0,
-	@LuxuryVehicleCharged bit = 0,
-	@AllowOperationCost bit = 0,
-	@AllowExtraFields bit = 0
-AS
-
-BEGIN
-	
-	begin try
-	
-		begin transaction
-	
-		update employers
-		set email = @email,
-			PhoneWork = @phone,
-			FAX = @fax,
-			PostalAddress1 = @address1,
-			PostalAddress2 = @address2,
-			PostalAddress3 = @address3,
-			PostalCity = @city,
-			PostalPostCode = @postcode,
-			PostalState = @state,
-			ParentID = @ParentCompanyID,
-			EmployerTypeId = @EmployerTypeId,
-			MaximumLeases = @MaximumLeases,
-			IsGstDeducted = @IsGstDeducted,
-			IsGstEquivalent = @IsGstEquivalent,
-			IsItcRefunded = @IsItcRefunded,
-			AllowLuxuryVehicle = @AllowLuxuryVehicle,
-			LuxuryVehicleCharged = @LuxuryVehicleCharged,
-			AllowOperationCost = @AllowOperationCost,
-			AllowExtraFields = @AllowExtraFields
-		where erid = @erid
-		
-		UPDATE EmployersExt
-		SET GSTPaidInDeduction = @gst, 
-			CollectFBT = @fbt, 
-			NovatedLeasingType = @quoting,
-			PartnerID = @partnerID,
-			Website = @Website,
-			Abn = @Abn,
-			Acn = @Acn,
-			AssignedTo = @AssignedTo,
-			Address = @Address,
-			City = @CityPhis,
-			PostCode = @PostCodePhis,
-			State = @StatePhis,
-			Industry = @Industry,
-			AboutCompany = @AboutCompany,
-			longitude = @longitude,
-			latitude = @latitude,
-			Active = @status,
-			Brokerid = @BrokerId,
-			AgentId = @AgentId,
-			PackagingProviderId = @PackagingProviderId
-		WHERE Erid =  @erid
-
-		commit transaction
-	end try
-	begin catch
-		if @@trancount > 0
-			rollback
-	
-		-- Raise an error with the details of the exception
-		DECLARE @ErrMsg nvarchar(4000), @ErrSeverity int
-		SELECT	@ErrMsg = ERROR_MESSAGE(),
-				@ErrSeverity = ERROR_SEVERITY()
-		
-		RAISERROR(@ErrMsg, @ErrSeverity, 1)
-	end catch
-END
 /****** ****************************************************************************** ******/
 
 /****** Object:  StoredProcedure [dbo].[sp_lp_UpdateQuoteCustomer]    Script Date: 20/12/2017 10:50:36 AM ******/
